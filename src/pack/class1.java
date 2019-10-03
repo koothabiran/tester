fcb6cef69546aaa1d9433763fa044d28200447a5
Your Name <you@example.com>
2019-10-03 14:51:36 +0530

    second commit

--- src/pack/class1.java
+++ src/pack/class1.java
@@ -1,5 +1,6 @@
 package pack;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -19,4 +20,23 @@ public class class1 {
 	  
 	  
   }
+  
+  @Test(priority=2)
+  public void login() throws InterruptedException
+	{
+	  //Enter username
+	  driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div/div[2]/div/form/div[1]/input")).sendKeys("8668030889");
+	  Thread.sleep(4000);
+	  
+	  //Enter Password
+	  driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div/div[2]/div/form/div[2]/input")).sendKeys("thulasi");
+	  Thread.sleep(2000);
+
+	  //click on login
+	  driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div/div[2]/div/form/div[3]/button")).click();
+	  Thread.sleep(2000);
+  
+}
+  
+  
 }
